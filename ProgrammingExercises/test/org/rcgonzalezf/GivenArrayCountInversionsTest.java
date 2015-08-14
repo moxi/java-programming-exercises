ffab43095f3591b7b6eb5cdc7689bf46f6c1287c
Roberto Carlos Gonzalez Flores <iscrobertogonzalez@gmail.com>
2015-08-14 00:31:40 -0700

    Adding UnitTest for CountInversions

---
+++ ProgrammingExercises/test/org/rcgonzalezf/GivenArrayCountInversionsTest.java
@@ -0,0 +1,52 @@
+package org.rcgonzalezf;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GivenArrayCountInversionsTest {
+
+    private GivenArrayCountInversions instaceToTest;
+    private int[] input;
+    private int numberOfInversions;
+
+    @Before
+    public void setUp() throws Exception {
+        instaceToTest = new GivenArrayCountInversions();
+    }
+
+    @Test
+    public void shouldReturnZeroForOrderedArray() throws Exception {
+        givenInputArray(new int[]{1, 2, 3, 6, 7});
+        whenCountInversions();
+        thenShouldBeInversionEqualsTo(0);
+    }
+
+    @Test
+    public void shouldReturnThreeForSampleArray() throws Exception {
+        givenInputArray(new int[]{1, 3,5,2,4, 6});
+        whenCountInversions();
+        thenShouldBeInversionEqualsTo(3);
+    }
+
+    @Test
+    public void shouldReturn15ForEntirelyUnsortedArrayOfSixEntries() throws Exception {
+        givenInputArray(new int[]{6, 5,4, 3,2,1});
+        whenCountInversions();
+        thenShouldBeInversionEqualsTo(15);
+    }
+
+    private void thenShouldBeInversionEqualsTo(int expected) {
+        assertEquals(expected, numberOfInversions);
+    }
+
+    private void whenCountInversions() {
+        numberOfInversions = instaceToTest.solution(input);
+    }
+
+    private void givenInputArray(int[] input) {
+        this.input = input;
+    }
+}

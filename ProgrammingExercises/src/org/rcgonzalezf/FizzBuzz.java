7b11484f5d7c9fbacb80baf33fc17cbae457c801
Roberto Carlos Gonzalez Flores <iscrobertogonzalez@gmail.com>
2015-08-17 22:01:29 -0700

    Adding fizz buzz implementation

---
+++ ProgrammingExercises/src/org/rcgonzalezf/FizzBuzz.java
@@ -0,0 +1,28 @@
+package org.rcgonzalezf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+
+    public List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>();
+        StringBuilder value = new StringBuilder();
+        for(int i = 1; i <=n ; ++i) {
+
+            if (i%3== 0) {
+                value.append("Fizz");
+            }
+            if (i%5==0) {
+                value.append("Buzz");
+            }
+
+            if (value.length() <= 0) {
+                value.append(i);
+                result.add(value.toString());
+            }
+            value.setLength(0);
+        }
+        return result;
+    }
+}

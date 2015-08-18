5c699b84f9444ed6e220f08a94e5f4b949a72439
Roberto Carlos Gonzalez Flores <iscrobertogonzalez@gmail.com>
2015-08-17 22:40:02 -0700

    Adding UnitTest for RansomNote

---
+++ ProgrammingExercises/test/org/rcgonzalezf/RansomNoteTest.java
@@ -0,0 +1,67 @@
+package org.rcgonzalezf;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RansomNoteTest {
+
+    private RansomNote instaceToTest;
+    private String magazine;
+    private String ransomNote;
+    private boolean canCreateRansomNote;
+
+    @Before
+    public void setUp() throws Exception {
+        instaceToTest = new RansomNote();
+        magazine = "Hello gray hair , are you going to put color on your gray window ?";
+    }
+
+    @Test
+    public void shouldReturnFalseForWordsNotInMagazine() throws Exception {
+        givenRansomNome("Color gray ");
+        whenCheckingThatRansomNoteCanBeCreated();
+        thenRansomNoteCanBeCreatedShouldBe(false);
+    }
+
+    @Test
+    public void shouldReturnTrueForWordsInMagazine() throws Exception {
+        givenRansomNome("color gray ");
+        whenCheckingThatRansomNoteCanBeCreated();
+        thenRansomNoteCanBeCreatedShouldBe(true);
+    }
+
+    @Test
+    public void shouldReturnFalseForNotEnoughWordsInMagazine() throws Exception {
+        givenRansomNome("color gray gray gray");
+        whenCheckingThatRansomNoteCanBeCreated();
+        thenRansomNoteCanBeCreatedShouldBe(false);
+    }
+
+    @Test
+     public void shouldReturnFalseForEmptyRansomNote() throws Exception {
+        givenRansomNome("");
+        whenCheckingThatRansomNoteCanBeCreated();
+        thenRansomNoteCanBeCreatedShouldBe(false);
+    }
+
+    @Test
+    public void shouldReturnFalseForRansomNoteWithMoreWordsThanMagazine() throws Exception {
+        givenRansomNome(magazine + "extra words");
+        whenCheckingThatRansomNoteCanBeCreated();
+        thenRansomNoteCanBeCreatedShouldBe(false);
+    }
+
+    private void thenRansomNoteCanBeCreatedShouldBe(boolean expected) {
+        assertEquals(expected, canCreateRansomNote);
+    }
+
+    private void whenCheckingThatRansomNoteCanBeCreated() {
+        this.canCreateRansomNote = instaceToTest.canRansom(ransomNote, magazine);
+    }
+
+    private void givenRansomNome(String ransomNote) {
+        this.ransomNote = ransomNote;
+    }
+}

c3cbe9e76901fb35f70ade2c278cdccb71650e5b
Roberto Carlos Gonzalez Flores <iscrobertogonzalez@gmail.com>
2015-08-19 09:52:44 -0700

    OneChangeApart UnitTest

---
+++ ProgrammingExercises/test/org/rcgonzalezf/GivenTwoStringsDetermineIfTheyAreOneChangeApartTest.java
@@ -0,0 +1,75 @@
+package org.rcgonzalezf;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GivenTwoStringsDetermineIfTheyAreOneChangeApartTest {
+
+    private GivenTwoStringsDetermineIfTheyAreOneChangeApart instanceUnderTest;
+    private String stringTwo;
+    private String stringOne;
+    private boolean oneEditApart;
+
+    @Before
+    public void setUp() throws Exception {
+        instanceUnderTest = new GivenTwoStringsDetermineIfTheyAreOneChangeApart();
+    }
+
+    @Test
+    public void shouldReturnTrueForOneReplacementCase() throws Exception {
+        givenTwoString("cat", "cut");
+        whenCheckingIfTheyAreOneEditApart();
+        thenShouldOneEditApartShouldBe(true);
+    }
+
+    @Test
+    public void shouldReturnFalseForTwoEditsApart() throws Exception {
+        givenTwoString("cat", "cuts");
+        whenCheckingIfTheyAreOneEditApart();
+        thenShouldOneEditApartShouldBe(false);
+    }
+
+    @Test
+    public void shouldReturnTrueForOneRemovalOrInsertionFirstStringCase() throws Exception {
+        givenTwoString("cat", "cats");
+        whenCheckingIfTheyAreOneEditApart();
+        thenShouldOneEditApartShouldBe(true);
+    }
+
+    @Test
+    public void shouldReturnTrueForOneRemovalOrInsertionSecondStringCase() throws Exception {
+        givenTwoString("cat", "ct");
+        whenCheckingIfTheyAreOneEditApart();
+        thenShouldOneEditApartShouldBe(true);
+    }
+
+    @Test
+    public void shouldReturnFalseForMoreThanOneRemovalOrInsertionFirstStringCase() throws Exception {
+        givenTwoString("cat", "cars");
+        whenCheckingIfTheyAreOneEditApart();
+        thenShouldOneEditApartShouldBe(false);
+    }
+
+    @Test
+    public void shouldReturnFalseForMoreThanOneRemovalOrInsertionSecondStringCase() throws Exception {
+        givenTwoString("core", "car");
+        whenCheckingIfTheyAreOneEditApart();
+        thenShouldOneEditApartShouldBe(false);
+    }
+
+    private void thenShouldOneEditApartShouldBe(boolean expected) {
+        assertEquals(expected, oneEditApart );
+    }
+
+    private void whenCheckingIfTheyAreOneEditApart() {
+        this.oneEditApart = instanceUnderTest.areOneEditApart(stringOne, stringTwo);
+    }
+
+    private void givenTwoString(String stringOne, String stringTwo) {
+        this.stringOne = stringOne;
+        this.stringTwo = stringTwo;
+    }
+}
